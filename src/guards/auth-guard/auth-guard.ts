aa3706f6d0911be17e0a9afaebcdfcce2b434307
Luca Parisi <lucaparisi.1999@outlook.it>
2026-05-25 14:53:16 +0200

    Routes - defined basic routes + angular configuration for authentication, authguard and interceptors

---
+++ src/guards/auth-guard/auth-guard.ts
@@ -0,0 +1,14 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+import {AuthService} from '@services';
+import {map, take} from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const auth   = inject(AuthService);
+  const router = inject(Router);
+
+  return auth.isLoggedIn().pipe(
+    take(1),
+    map(isLoggedIn => isLoggedIn ? true : router.createUrlTree(['/auth/login']))
+  );
+};
