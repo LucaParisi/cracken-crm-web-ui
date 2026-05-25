c2faa1ce61d1dad71464dfd41f57d70bd62191aa
Luca Parisi <lucaparisi.1999@outlook.it>
2026-05-25 14:54:55 +0200

    Project structure - created index file for easy access modules

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export * from './dashboard-page/dashboard-page';
+export * from './forgot-password-page/forgot-password-page';
+export * from './login-page/login-page';
+export * from './sign-up-page/sign-up-page';
+export * from './not-found/not-found';
