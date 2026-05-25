1f17735310e1d9f0b49628c0afe902a011b5e5d8
Luca Parisi <lucaparisi.1999@outlook.it>
2026-05-25 14:56:12 +0200

    Generic - deleted footer against not-found page, created types for combox and gender constants options

---
+++ src/constants/gender.constants.ts
@@ -0,0 +1,7 @@
+import {ComboxOption} from '../types/combox.type';
+
+export const AVAILABLE_GENDERS: ComboxOption[] = [
+  { label: 'Male', value: 'M'},
+  { label: 'Female', value: 'F'},
+  { label: 'Prefer not to say', value: 'N/D'}
+]
